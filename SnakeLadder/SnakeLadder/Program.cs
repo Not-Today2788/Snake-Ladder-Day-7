248fe1d8b1beba3caab098f1145dd8ee44257767
Prakhar <prakhar2788@gmail.com>
2024-01-20 00:22:34 +0530

    [Prakhar] Add: Added the 1st Use Case Program of Snake Ladder

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startplayer= 0;
+            Console.WriteLine("Welcome to Snake Ladder Game");
+            
+            Console.ReadLine();
+        }
+    }
+}
