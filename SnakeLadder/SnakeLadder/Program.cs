c5e8c8630f865e48cd294acbed401e76883bedd0
Prakhar <prakhar2788@gmail.com>
2024-01-20 01:50:20 +0530

    [Prakhar] Add: Added the 2nd Use Case Program of Snake Ladder

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,39 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            int startplayer= 0;
-            Console.WriteLine("Welcome to Snake Ladder Game");
-            
+            bool play;
+            play = wannaplay();
+            if (play)
+            do
+            {
+                if(play)
+                {
+                    
+                   
+                        if (play)
+                        {
+                            Random rnd = new Random();
+                            int ran = rnd.Next(1, 6);
+                            Console.WriteLine("\n"+ran+"\n");
+                            play = wannaplay();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                }
+            } while (play);
             Console.ReadLine();
+
+
+
+
+        }
+        static bool wannaplay()
+        {
+            Console.WriteLine("You wanna play?? (Yes/No) : \n");
+            string userinput = Console.ReadLine().ToLower();
+            return userinput == "yes";
         }
     }
 }
